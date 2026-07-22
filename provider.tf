14db58cca5dcebba78192addb0a3be784a65783d
venkatapravalika200013-dev <venkatapravalika200013@gmail.com>
2026-07-22 13:11:51 +0530

    Add Terraform provider configuration for AWS

---
+++ provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = var.aws_region
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true # Essential for LocalStack S3 resolution
+
+  endpoints {
+    s3  = "http://localhost:4566"
+    ec2 = "http://localhost:4566"
+    iam = "http://localhost:4566"
+  }
+}
